6b7007f7567f51ef32ed10e516dde4759f153adb
eonmilu <eonmilu@gmail.com>
2018-08-22 20:43:41 +0200

    Add status codes, insert function

--- public/yourtime/auth/google.js
+++ public/yourtime/auth/google.js
@@ -1,6 +1,14 @@
 const VALIDATE_TOKEN_URL = "https://oxygenrain.com/yourtime/auth/validate";
 const REMOVE_TOKEN_URL = "https://oxygenrain.com/yourtime/auth/remove"
 const DEFAULT_TIMEOUT = 1500;
+const STATUS_CODE = {
+    FOUND: "200",
+    NOT_FOUND: "210",
+    ERROR: "220",
+    BAD_LOGIN: "230",
+    OK: "240"
+};
+
 
 function onSignIn(googleUser) {
     const profile = googleUser.getBasicProfile();
@@ -39,7 +47,7 @@ function onSignIn(googleUser) {
     });
 }
 
-function signOut() {
+function onSignOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         const token = Cookies.get("yourtime-token-server");
@@ -53,12 +61,23 @@ function signOut() {
             content: true
         }).appendTo("head");
 
-        const removed = removeServerToken(token);
-        if (removed) {
-            console.log('User signed out.');
-        } else {
-            console.log(`Unable to sign out`);
-        }
+        $.ajax({
+            method: "POST",
+            url: REMOVE_TOKEN_URL,
+            contentType: "application/x-www-form-urlencoded",
+            data: {
+                idtoken: token,
+            },
+            timeout: DEFAULT_TIMEOUT
+        }).then((content) => {
+            if (content == STATUS_CODE.OK) {
+                console.log("Sign out successful")
+            } else {
+                console.log(`Unable to delete token from server. Error: ${content}`);
+            }
+        }).fail((error) => {
+            console.log(`Unable to delete token from server. Error: ${error}`);
+        });
     }, function (error) {
         console.log(`Unable to sign out: ${error}`);
     });
@@ -66,20 +85,4 @@ function signOut() {
 
 function isStatusCode(response) {
     return response.length <= 3;
-}
-
-function removeServerToken(token) {
-    $.ajax({
-        method: "POST",
-        url: REMOVE_TOKEN_URL,
-        contentType: "application/x-www-form-urlencoded",
-        data: {
-            idtoken: idToken,
-        },
-        timeout: DEFAULT_TIMEOUT
-    }).then(() => {
-        return true;
-    }).fail(() => {
-        return false;
-    })
 }
